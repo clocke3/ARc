f4e5a352f2f51ed414939d9d81026000024314cc
aaazmah <hsheikh38@gmail.com>
2018-07-06 12:44:01 -0400

    Roles

---
+++ ARc_Unity/Assets/Scripts/Roles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Role : MonoBehaviour {
+
+    string Name;
+    List<Employee> employees; 
+    Division division;
+
+    public Role() {
+        
+    }
+
+    public Role(string name, Division division, List<Employee> employees)
+    {
+        Name = name;
+        Division Division = division;
+        List<Employee> Employee = employees; 
+
+    }
+
+    public string getName() {
+        Name = name; 
+    }
+
+    public Division getDivision() {
+        Division Division = division;
+    }
+
+    public List<Employee> getEmployees() {
+        List<Employee> Employee = employees;
+    }
+
+
+
+}
