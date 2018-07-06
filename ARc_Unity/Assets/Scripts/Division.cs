a9ae18b90e539f7b9cd2d7c06dc4db7c8736ee97
Andrew George <ageorge@vassar.edu>
2018-07-06 09:56:50 -0400

    added Division

---
+++ ARc_Unity/Assets/Scripts/Division.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Division : MonoBehaviour {
+
+    // data
+    private string name;
+    private Department department;
+    private List<Role> roles;
+
+
+    // constructor
+    public Division(string name, Department department, List<Role> roles) {
+        this.name = name;
+        this.department = department;
+        this.roles = roles;
+    }
+
+
+    // getters
+    public string getName() {
+        return name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+}
