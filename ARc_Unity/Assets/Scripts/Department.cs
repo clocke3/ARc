936b0d63e982361cd62d9a71faed5cd8837f654b
Chihea Locke <clocke3@ycp.edu>
2018-07-06 11:14:40 -0400

    Department Base Class

---
+++ ARc_Unity/Assets/Scripts/Department.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Department : Profiable
+{
+    public string description;
+    public List<string> divisions; 
+    private string name;
+    private int galleryID;
+
+    public string getDescription(){
+        return description;
+    }
+    public List<string> getDivisions(){
+        return divisions;
+    }
+    private string getName(){
+        return name;
+    }
+    private int getGalleryID(){
+        return galleryID;
+    }
+}
