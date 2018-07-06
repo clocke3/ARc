52b0d4bad0324bd9bda0c995c401adc15a53aa2b
Chihea Locke <clocke3@ycp.edu>
2018-07-06 11:30:09 -0400

    slight changes

--- ARc_Unity/Assets/Scripts/Department.cs
+++ ARc_Unity/Assets/Scripts/Department.cs
@@ -4,10 +4,17 @@ using System.Collections.Generic;
 
 public class Department : Profiable
 {
-    public string description;
-    public List<string> divisions; 
+    private string description;
+    private List<string> divisions;
     private string name;
     private int galleryID;
+
+    private Department (name, description, divisions, galleryID){
+        this.name = name;
+        this.description = description;
+        this.divisions = divisons;
+        this.galleryID = galleryID;
+    }
 
     public string getDescription(){
         return description;
