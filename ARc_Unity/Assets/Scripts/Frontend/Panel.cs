3fc278775a6d19eb7eda6b51ca3249ef95aef05c
Andrew George <ageorge@vassar.edu>
2018-07-10 16:04:28 -0400

    panel and ProfileManager

---
+++ ARc_Unity/Assets/Scripts/Frontend/Panel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Panel : MonoBehaviour {
+
+    // variables
+    protected ProfileManager profileManager;
+    public DatabaseObjectButton databaseObjectButton;
+
+    // setters
+    public void setup(ProfileManager profileManager, DatabaseObject databaseObject) {
+        this.profileManager = profileManager;
+        setToRepresent(databaseObject);
+        setAttributes();
+    }
+
+    protected abstract void setToRepresent(DatabaseObject databaseObject); // see if the object is compatible and if so set that up
+
+    protected abstract void setAttributes(); // if there's an object to represent, then set up each attribute for visual profile
+
+}
--- ARc_Unity/Assets/Scripts/Frontend/ProfileManager.cs
+++ ARc_Unity/Assets/Scripts/Frontend/ProfileManager.cs
@@ -7,7 +7,8 @@ public class ProfileManager : MonoBehaviour {
     // variables
     private DatabaseManager databaseManager;
     public ItemManager itemManager;
-    public List<GameObject> panels;
+    private List<Panel> panels;
+    public Panel[] panelPrefabs = new Panel[5];
 
     // initialization
     void Start() {
@@ -27,8 +28,31 @@ public class ProfileManager : MonoBehaviour {
     }
 
     // adding panels
-    public void addPanel(DatabaseObject databaseObject) {
-        
+    public void addPanel(DatabaseObject databaseObject)
+    {
+        Panel newPanel;
+        switch (databaseObject.getTypeID())
+        {
+            case DatabaseObject.DEPARTMENT:
+                newPanel = Instantiate(panelPrefabs[DatabaseObject.DEPARTMENT], transform.position, Quaternion.identity) as Panel;
+                break;
+            case DatabaseObject.DIVISION:
+                newPanel = Instantiate(panelPrefabs[DatabaseObject.DIVISION], transform.position, Quaternion.identity) as Panel;
+                break;
+            case DatabaseObject.ROLE:
+                newPanel = Instantiate(panelPrefabs[DatabaseObject.ROLE], transform.position, Quaternion.identity) as Panel;
+                break;
+            case DatabaseObject.EMPLOYEE:
+                newPanel = Instantiate(panelPrefabs[DatabaseObject.EMPLOYEE], transform.position, Quaternion.identity) as Panel;
+                break;
+            case DatabaseObject.MEDIAGALLERY:
+                newPanel = Instantiate(panelPrefabs[DatabaseObject.MEDIAGALLERY], transform.position, Quaternion.identity) as Panel;
+                break;
+            default:
+                return;
+        }
+        newPanel.setup(this, databaseObject);
+        panels.Add(newPanel);
     }
 
     public void displayProfile(string qrID) {
