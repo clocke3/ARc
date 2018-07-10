e77713a855fae09ce2d3d780b2dd29708e840d70
Andrew George <ageorge@vassar.edu>
2018-07-10 10:27:11 -0400

    databaseObject and profiable classes

--- ARc_Unity/Assets/Scripts/Backend/DatabaseObject.cs
+++ ARc_Unity/Assets/Scripts/Backend/DatabaseObject.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public abstract class DatabaseObject : MonoBehaviour {
 
+    // variables
     string label;
 
+    // constructor
     protected DatabaseObject(string label) {
         this.label = label;
     }
 
+    // getters
     public string getLabel() {
         return label;
     }
--- ARc_Unity/Assets/Scripts/Backend/Profiable.cs
+++ ARc_Unity/Assets/Scripts/Backend/Profiable.cs
@@ -5,19 +5,23 @@ using System.Collections.Generic;
 public abstract class Profiable : DatabaseObject
 {
 
+    // variables
     public const int DEPARTMENT = 0, EMPLOYEE = 1;
     protected string qrID;
     protected int typeID;
     protected MediaGallery gallery;
 
+    // constructor
     protected Profiable(string label, string qrID, 
                         MediaGallery gallery) : base(label) {
         this.qrID = qrID;
         this.gallery = gallery;
     }
 
+    // setters
     protected abstract void setTypeID();
 
+    // getters
     public int getTypeID(){
         return typeID;
     }
@@ -28,6 +32,11 @@ public abstract class Profiable : DatabaseObject
 
     public string getQRID(){
         return qrID;
+    }
+
+    // comparison
+    public bool equalQRID(string otherQRID) {
+        return string.Equals(qrID, otherQRID);
     }
 }
 
