1ecaa7f517b13d280aa924b39e2574775738c6dc
Jon <jonathan.md@outlook.com>
2018-07-12 15:37:58 -0400

    WORKS

--- ARc_Unity/Assets/Scripts/Backend/DatabaseManager.cs
+++ ARc_Unity/Assets/Scripts/Backend/DatabaseManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Data.Sqlite; 
+using System.Data; 
+using System;
 
 public class DatabaseManager : MonoBehaviour {
 
@@ -22,10 +25,29 @@ public class DatabaseManager : MonoBehaviour {
         // set up database variable
 	}
 
-    void Start() {
-        dataToObjects();
-    }
-
+    void Start () {
+    string conn = "URI=file:" + Application.dataPath + "/test.db"; //Path to database.
+     IDbConnection dbconn;
+     dbconn = (IDbConnection) new SqliteConnection(conn);
+     dbconn.Open(); //Open connection to the database.
+     IDbCommand dbcmd = dbconn.CreateCommand();
+     string sqlQuery = "SELECT LAST_NAME,FIRST_NAME,POSITION " + "FROM EMPLOYEE";
+     dbcmd.CommandText = sqlQuery;
+     IDataReader reader = dbcmd.ExecuteReader();
+     while (reader.Read())
+     {
+         string in_name = reader.GetString(0)+reader.GetString(1);
+         string pos = reader.GetString(2);
+        
+         Debug.Log("  name ="+in_name+"  position ="+  pos);
+     }
+     reader.Close();
+     reader = null;
+     dbcmd.Dispose();
+     dbcmd = null;
+     dbconn.Close();
+     dbconn = null;
+ }
 
     // setters
     private void dataToObjects() {
