d71cf611c5abc2b54173737cdeec58bfabecb79e
Andrew George <ageorge@vassar.edu>
2018-07-11 23:26:11 -0400

    scan manager skeleton

--- ARc_Unity/Assets/Scripts/Frontend/Managers/ScanManager.cs
+++ ARc_Unity/Assets/Scripts/Frontend/Managers/ScanManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class ScanManager : ItemManager {
 
-	
+    private void Update()
+    {
+        // each frame, check to see if a qr code is being seen and act accordingly
+    }
 
+    private bool isQR() {
+        // checks if the current image is a qr code
+        return false;
+    }
+
+    private bool isValid() {
+        // checks to see if the current qr code is valid
+        return false;
+    }
+
+    IEnumerator verify() {
+        // coroutine to check if the same qr code is held for a specified amount of time
+        return null;
+    }
 }
