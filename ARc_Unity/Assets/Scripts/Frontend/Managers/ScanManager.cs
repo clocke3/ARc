ad3f061db1c4a05f548955141649b64c1c1d7898
Andrew George <ageorge@vassar.edu>
2018-07-11 23:31:02 -0400

    search manager sketch

--- ARc_Unity/Assets/Scripts/Frontend/Managers/ScanManager.cs
+++ ARc_Unity/Assets/Scripts/Frontend/Managers/ScanManager.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class ScanManager : ItemManager {
 
+    // variables
+    private int verificationTime = 5;
+
+    // frame-by-frame functions
     private void Update()
     {
         // each frame, check to see if a qr code is being seen and act accordingly
     }
 
+    // checkers
     private bool isQR() {
         // checks if the current image is a qr code
         return false;
@@ -20,7 +25,7 @@ public class ScanManager : ItemManager {
     }
 
     IEnumerator verify() {
-        // coroutine to check if the same qr code is held for a specified amount of time
+        // coroutine to check if the same qr code is held for verificationTime number of seconds
         return null;
     }
 }
--- ARc_Unity/Assets/Scripts/Frontend/Managers/SearchManager.cs
+++ ARc_Unity/Assets/Scripts/Frontend/Managers/SearchManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SearchManager : ItemManager {
 
-	
+    // variables
+    private string keyword;
+
 
 }
